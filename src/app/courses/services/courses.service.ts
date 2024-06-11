8e1e2addedcefcc3b28d69b050b1716660bf52c4
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-06-11 10:30:08 -0300

    Teste http com json e RXJS

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,18 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Course } from '../model/course';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API= '/assets/courses.json';
+
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] { //método para retornar uma lista de curso para o componente
-    return [
-      { _id:'1', name:'Angular', category:'front-end' }
-    ];
+  list() { //método para retornar uma lista de curso para o componente
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      tap(courses => console.log(courses))
+    );
   }
-
 }
