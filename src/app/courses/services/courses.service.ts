634b9ab041929f482b3fad921d94bd42b14fb8b1
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-06-11 11:18:05 -0300

    spinner added, some css changes

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Course } from '../model/course';
-import { tap } from 'rxjs/operators';
+import { delay, first, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +16,8 @@ export class CoursesService {
   list() { //método para retornar uma lista de curso para o componente
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
+      first(), //fala pro rxjs que assim que vem a primeira resposta pode se desinscrever do observable
+      delay(5000),
       tap(courses => console.log(courses))
     );
   }
