b3bf3c9b1cb3914c02df1ce9ccb752d52264c839
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-07-18 16:56:50 -0300

    validações front

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 
@@ -17,8 +17,10 @@ export class CourseFormComponent implements OnInit {
 
   form = this.formBuilder.group({
     _id: [''],
-    name: [''],
-    category: ['']
+    name: ['', [Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(100)]],
+    category: ['', Validators.required]
   });
 
   constructor(private formBuilder: NonNullableFormBuilder,
@@ -54,6 +56,25 @@ export class CourseFormComponent implements OnInit {
 
   private onError(){
     this.snackBar.open('Erro ao salvar curso', '', {duration: 5000});
+  }
+
+  public getErrorMessage(fieldName: string){
+    const field = this.form.get(fieldName);
+
+    if (field?.hasError('required')){ //1º Validação - se foi preenchido
+      return 'Campo obrigatório';
+    }
+    if (field?.hasError('minlength')){ //2º Validação - tem + de 5 caracteres
+      const requiredLength: number = field.errors ? field.errors['minlength']['requiredLength'] : 5;
+      return `Tamanho mínimo precisa ser de ${requiredLength} caracteres`;
+    }
+    if (field?.hasError('maxlength')){ //3º Validação - tem menos de 100 caracteres
+      const requiredLength: number = field.errors ? field.errors['maxlength']['requiredLength'] : 100;
+      return `Tamanho máximo de ${requiredLength} caracteres excedido`;
+    }
+
+    //esse final é como se fosse meu ELSE
+    return 'Campo inválido'
   }
 
 }
